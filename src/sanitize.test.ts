854ce1335fb41d717672786dbe28e2918c5e5039
André Gaul <andre@gaul.io>
2018-05-02 20:00:50 -0400

    sanitize: make tests return void

--- src/sanitize.test.ts
+++ src/sanitize.test.ts
@@ -4,9 +4,13 @@ import * as sanitize from './sanitize';
 
 describe('sanitize', () => {
   describe('boolean()', () => {
-    it('should sanitize true', () => expect(sanitize.boolean('true')).to.equal(true));
+    it('should sanitize true', () => {
+      expect(sanitize.boolean('true')).to.equal(true);
+    });
 
-    it('should sanitize false', () => expect(sanitize.boolean('false')).to.equal(false));
+    it('should sanitize false', () => {
+      expect(sanitize.boolean('false')).to.equal(false);
+    });
 
     it('should throw if not a boolean', () => {
       expect(() => sanitize.boolean('1')).to.throw('Value 1 is not a boolean.');
@@ -27,7 +31,9 @@ describe('sanitize', () => {
   });
 
   describe('json()', () => {
-    it('should sanitize a JSON string', () => expect(sanitize.json('{"foo": 1}')).to.eql({foo: 1}));
+    it('should sanitize a JSON string', () => {
+      expect(sanitize.json('{"foo": 1}')).to.eql({foo: 1});
+    });
 
     it('should throw if not a JSON string', () => {
       expect(() => sanitize.json('foo')).to.throw('Unexpected token');
@@ -35,9 +41,13 @@ describe('sanitize', () => {
   });
 
   describe('number()', () => {
-    it('should sanitize a number', () => expect(sanitize.number('1.337')).to.eql(1.337));
+    it('should sanitize a number', () => {
+      expect(sanitize.number('1.337')).to.eql(1.337);
+    });
 
-    it('should sanitize an integer', () => expect(sanitize.number('-2')).to.eql(-2));
+    it('should sanitize an integer', () => {
+      expect(sanitize.number('-2')).to.eql(-2);
+    });
 
     it('should throw if not a number', () => {
       expect(() => sanitize.number('foo')).to.throw('Value foo is not a number.');
@@ -45,6 +55,8 @@ describe('sanitize', () => {
   });
 
   describe('string()', () => {
-    it('should return the input', () => expect(sanitize.string('foo')).to.equal('foo'));
+    it('should return the input', () => {
+      expect(sanitize.string('foo')).to.equal('foo');
+    });
   });
 });
